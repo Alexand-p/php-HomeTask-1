1bc819994d7aa7e3e172c74fdd6517a855b46260
Alexand-p <3082303@gmail.com>
2022-02-08 13:36:29 +0300

    init:index.php

--- Class/index.php
+++ Class/index.php
@@ -1,1 +1,11 @@
 <?php
+class Student{
+    public $age = 14;
+    public $name = 'Alex';
+    public function printData()
+    {
+        echo $this->name;
+        echo $this->age;
+    }
+}
+echo  'printData';
