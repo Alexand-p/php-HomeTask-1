acffa1d83c4167fb5e7588993b3baf65f1fe2262
Alexand-p <3082303@gmail.com>
2022-02-10 12:43:15 +0300

    finish

--- index.php
+++ index.php
@@ -2,22 +2,31 @@
 $uploadsDir = './downloads/';
 if(isset($_FILES) && isset($_FILES['uploadedFile'])){
      $fileData = $_FILES['uploadedFile'];
-     if ($fileData['error'] ===  UPLOAD_ERR_OK){
-         $fileName = $fileData['name'];
-         $tmpName = $fileData['tmp_name'];
-         $destinationPath = $uploadsDir . $fileName;
-         if(move_uploaded_file($tmpName, $destinationPath)){
-             header('Refresh: 2; url=http://learn/titel.php');
-             echo "Привет!Через 2 секунд вас переместит  на страницу с фотографиями ))";
-
-         }
-         else{
-             echo  'ERROR:cant upload file';
-             die;
-         }
-     }
+    if($_FILES['uploadFile']['size'] > 4*1024*1024) {
+        exit("Файл слишком большой");
+    }
+    else{
+        if ($fileData['error'] ===  UPLOAD_ERR_OK){
+            $fileName = $fileData['name'];
+            $tmpName = $fileData['tmp_name'];
+            $destinationPath = $uploadsDir . $fileName;
+            if(move_uploaded_file($tmpName, $destinationPath)){
+                header('Refresh: 2; url=http://learn/titel.php');
+                echo "Привет!Через 2 секунд вас переместит  на страницу с фотографиями ))";
+            }
+            else{
+                echo  'ERROR:cant upload file';
+                die;
+            }
+        }
+    }
 }
+
     $html = include_once 'index.html';
+
+
+
+
 
 
 
--- titel.php
+++ titel.php
@@ -2,24 +2,21 @@
 //require_once 'redirect.html';
 
 $directory = "downloads";
-if($handle = opendir($directory)){
+//if($handle = opendir($directory)){
+//    while(false !== ($file = readdir($handle))) {
+//        if($file != "." && $file != ".."){
+//            echo $file."<br>";
+//        }
+//    }
+//}
 
-    while(false !== ($file = readdir($handle))) {
-        if($file != "." && $file != ".."){
-            echo $file."<br>";
-        }
-    }
-}
-
-   // Папка с изображениями
 $allowed_types=array("jpg", "png", "gif", "jpeg");
 $file_parts = array();
 $ext="";
 $title="";
 $i=0;
-//пробуем открыть папку
 $dir_handle = @opendir($directory) or die("Ошибка при открытии папки !!!");
-while ($file = readdir($dir_handle))    //поиск по файлам
+while ($file = readdir($dir_handle))
 {
     if($file=="." || $file == "..") continue;
     $file_parts = explode(".",$file);
@@ -27,11 +24,11 @@ while ($file = readdir($dir_handle))    //поиск по файлам
 
     if(in_array($ext,$allowed_types))
     {
-        echo '<div class = "blok_img" >
-                <img src="'.$directory.'/'.$file.'" class="pimg" title="'.$file.'"  style="margin-top: 10px"/>
+        echo '<div class = "img-container" >
+                <img src="'.$directory.'/'.$file.'" class="png" title="'.$file.'"  alt="#" style="margin-top: 10px"/>
             </div>';
         $i++;
     }
 
 }
-closedir($dir_handle);  
+closedir($dir_handle);  //закрыть папку
