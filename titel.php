e372c12ac6ee3fac18a40c6d28610c4019a1e43f
Alexand-p <3082303@gmail.com>
2022-02-10 14:34:31 +0300

    finish

--- titel.php
+++ titel.php
@@ -1,34 +1,32 @@
 <?php
-//require_once 'redirect.html';
 
-$directory = "downloads";
-//if($handle = opendir($directory)){
+$imageType=["jpg", "png", "gif", "jpeg"];
+$dir = "downloads";
+$fileParts = [];
+$ext='';
+$i = 0;
+//if($handle = opendir($dir)){
 //    while(false !== ($file = readdir($handle))) {
-//        if($file != "." && $file != ".."){
+//        if($file != '.' && $file != '..'){
 //            echo $file."<br>";
 //        }
 //    }
 //}
 
-$allowed_types=array("jpg", "png", "gif", "jpeg");
-$file_parts = array();
-$ext="";
-$title="";
-$i=0;
-$dir_handle = @opendir($directory) or die("Ошибка при открытии папки !!!");
-while ($file = readdir($dir_handle))
+$dirHandle = opendir($dir) ;
+while ($file = readdir($dirHandle))
 {
-    if($file=="." || $file == "..") continue;
-    $file_parts = explode(".",$file);
-    $ext = strtolower(array_pop($file_parts));
+    if($file=='.' || $file == '..') continue;
+    $fileParts = explode('.',$file);
+    $ext = strtolower(array_pop($fileParts));
 
-    if(in_array($ext,$allowed_types))
+    if(in_array($ext,$imageType))
     {
         echo '<div class = "img-container" >
-                <img src="'.$directory.'/'.$file.'" class="png" title="'.$file.'"  alt="#" style="margin-top: 10px"/>
+                <img src="'.$dir.'/'.$file.'" class="png" title="'.$file.'"  alt="#" style="margin-top: 10px"/>
             </div>';
         $i++;
     }
 
 }
-closedir($dir_handle);  //закрыть папку
+closedir($dirHandle);
